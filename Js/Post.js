e404946d5e1cb746e97009b931d01c5f48b1b00f
hungmanh <hungmanh0621@gmail.com>
2023-12-20 16:00:41 +0700

    kkkkkk

--- Js/Post.js
+++ Js/Post.js
@@ -726,6 +726,7 @@ $(document).on('click', '.reply-comment-btn', function (e) {
     if (msg.trim().length == 0) {
         showNotification("Please enter text to reply comment!")
     } else {
+        $("#post-details-modal .view-reply-btn[commentid='"+commentID+"']").data("next-offset", 0);
         createReplyComment(commentID, postID, userID, msg);
     }
 });
@@ -741,6 +742,7 @@ $(document).on("keyup", ".reply-comment-textbox", function (e) {
         if (msg.trim().length == 0) {
             showNotification("Please enter text to comment!")
         } else {
+            $("#post-details-modal .view-reply-btn[commentid='"+commentID+"']").data("next-offset", 0);
             createReplyComment(commentID, postID, userID, msg);
         }
     }
@@ -809,6 +811,7 @@ $(document).on("click",".view-reply-btn", function (e){
                     if (response.trim() != "[]" && response.trim() != "0") {
                         var cmtData = JSON.parse(response);
                         console.log(cmtData);
+                        if (offset == 0) $(".reply-comment-msg[commentid='" + commentid + "']").html("");
                         $.each(cmtData, function (key, value) {
                             var time = timeAgo(value.cmt["date"]);
                             $("#post-details-modal .view-reply-btn").data("next-offset", nextOffet);
@@ -832,10 +835,10 @@ $(document).on("click",".view-reply-btn", function (e){
                         if (nextOffet > cmtData[0].totalComment) nextOffet = cmtData[0].totalComment;
                         var remainingQuantity = cmtData[0].totalComment - nextOffet;
                         $("#post-details-modal .view-reply-btn[commentid='"+commentid+"']").data("next-offset", nextOffet);
-                        $("#post-details-modal .count-cmtr[commentid='"+commentid+"']").remove();
-                        $("#post-details-modal .view-reply-btn[commentid='"+commentid+"']").after(`<span class="count-cmtr" commentid='3'>${nextOffet}/${cmtData[0].totalComment}</span>`);
                         $("#post-details-modal .view-reply-btn[commentid='"+commentid+"']").next().attr('commentid', commentid);
-
+                        $("#post-details-modal .view-reply-btn[commentid='"+commentid+"']").text(`View replies (${remainingQuantity})`)
+                        $("#post-details-modal .count-cmtr").remove();
+                        $("#post-details-modal .view-reply-btn[commentid='"+commentid+"']").after(`<span class="count-cmtr" >${nextOffet}/${cmtData[0].totalComment}</span>`);
                         // if (remainingQuantity <= 0) {
                         //     $("#post-details-modal .view-reply-btn[commentid='"+commentid+"']").hide();
                         // }
